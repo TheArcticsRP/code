d13573e5a64d6300195b8516d853b64d155c31e3
TheArcticsRP <141246453+TheArcticsRP@users.noreply.github.com>
2023-10-26 23:40:29 -0500

    Create file.js

---
+++ file.js
@@ -0,0 +1,25 @@
+# Combat Update is here
+
+```ansi
+[2;33m» [0m[2;37mLink to our forums if you wish to read it there![0m
+
+```
+[Combat Update Changelog](https://www.glorified.online/forums/index.php?/blogs/entry/2-combat-update-001/)
+
+```ansi
+[2;33m» [0m[2;37mAs well as our store if you are thinking about purchasing a rank![0m
+
+```
+[Glorified Store](https://glorified.tebex.io)
+
+```ansi
+[2;33m» [0m[2;37mIf you encounter any bugs, please let us know using our Discord Bug forums or our official Forums![0m
+
+```
+[Discord Bug Forums](https://discord.com/channels/1112456621055037502/1149064349617487902)
+[Official Forums Bugs](https://discord.com/channels/1112456621055037502/1149064349617487902)
+
+```ansi
+[2;33m[1;33m[0m[2;33m[1;33m[1;33m[1;34m» Thank you all for your support through our growth![0m[1;33m[0m[1;33m[0m[2;33m[0m
+
+```
